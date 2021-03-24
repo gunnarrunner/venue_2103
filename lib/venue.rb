cfadeaa718ec98144a54464a4cf59df449780ef2
Gunnar <dunkcrunk.com>
2021-03-24 15:51:16 -0600

    Iteration 4 complete.

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,33 @@
 class Venue
+
+    attr_reader :name, :capacity, :patrons
+    def initialize(name, capacity)
+        @name = name 
+        @capacity = capacity
+        @patrons = []
+    end
+
+    def add_patron(patron)
+       @patrons << patron 
+    end
+
+    def yell_at_patrons
+        @patrons.map do |patron|
+            patron.upcase
+        end
+    end
+
+    def over_capacity?
+        if @patrons.length > @capacity
+            true
+        else
+            false
+        end
+    end
+
+    def kick_out
+        while over_capacity? == true
+            @patrons.pop
+        end
+    end
 end
